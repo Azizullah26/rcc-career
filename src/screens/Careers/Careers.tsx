65ee44cf4c0ea86e9fe3a83c296963f3ca2640f0
Azizullah26 <74201681+Azizullah26@users.noreply.github.com>
2025-08-05 20:02:56 +0400

    Make EL RACE CAREERS text smaller and inline for mobile

--- src/screens/Careers/Careers.tsx
+++ src/screens/Careers/Careers.tsx
@@ -119,7 +119,7 @@ export const Careers = (): JSX.Element => {
           <div className="h-48 md:h-60 bg-[url(/rectangle-1.svg)] bg-cover bg-[50%_50%] absolute w-full">
             <div className="h-48 md:h-60 bg-[linear-gradient(90deg,rgba(0,7,69,0.8)_45%,rgba(84,93,179,0.8)_100%)] absolute w-full">
               <h1 className="absolute top-6 md:top-[26px] left-1/2 transform -translate-x-1/2 [font-family:'Times_New_Roman-Regular',Helvetica] font-normal text-white text-[24px] md:text-[42.9px] tracking-[0] leading-normal text-center px-4">
-                <span className="text-[20px] md:text-[48.1px] font-bold tracking-wider">EL RACE CAREERS</span>
+                <span className="text-[16px] md:text-[48.1px] font-bold tracking-wider whitespace-nowrap">EL RACE CAREERS</span>
               </h1>
 
               <p className="absolute w-[90%] md:w-[1056px] top-[80px] md:top-[102px] left-1/2 transform -translate-x-1/2 [font-family:'Tajawal',Helvetica] font-normal text-white text-[12px] md:text-[19.7px] text-justify tracking-[0] leading-normal px-4">
