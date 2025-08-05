e48785a6197ff8b9ef3688a413341bf9af48db95
Azizullah26 <74201681+Azizullah26@users.noreply.github.com>
2025-08-05 20:25:36 +0400

    Updated Careers.tsx

--- src/screens/Careers/Careers.tsx
+++ src/screens/Careers/Careers.tsx
@@ -122,7 +122,6 @@ export const Careers = (): JSX.Element => {
                 <span className="text-[16px] md:text-[48.1px] font-bold tracking-wider whitespace-nowrap">EL RACE CAREERS</span>
               </h1>
 
-              <p className="absolute w-[90%] md:w-[1056px] top-[45px] md:top-[85px] left-1/2 transform -translate-x-1/2 [font-family:'Tajawal',Helvetica] font-normal text-white text-[9px] md:text-[16px] text-center tracking-[0] leading-tight px-4 max-h-none">
               <p className="absolute w-[80%] md:w-[800px] top-[45px] md:top-[85px] left-1/2 transform -translate-x-1/2 [font-family:'Tajawal',Helvetica] font-normal text-white text-[9px] md:text-[16px] text-center tracking-[0] leading-tight px-4 max-h-none">
                 EL RACE&nbsp;&nbsp;is a well-established local company with over 40 years of experience, known for
                 delivering major government projects with excellence and reliability. We take pride in our talented and
